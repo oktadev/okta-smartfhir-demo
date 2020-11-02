d484b14cef476ad76c06fc1fbb967f96e2820154
Dan Cinnamon <dan.cinnamon@okta.com>
2020-11-02 18:49:38 +0000

    Updated the token hook to validate all aspects of the request (client_id, scopes, patient).

--- token_hook.js
+++ token_hook.js
@@ -15,9 +15,10 @@ app.post("/tokenhook", (request, response) => {
 	var authorizeUrl = request.body.data.context.request.url.value;
 	console.log('Token hook invoked with url: ' + authorizeUrl)
 
-	var regex = /state=([^&]+)/i;
-	var pickerContextJWT = authorizeUrl.match(regex)[1];
 	var verifiedPickerContextJWT = '';
+	var pickerContextJWT = request.body.data.context.protocol.request.state;
+	var scope = request.body.data.context.protocol.request.scope;
+	var client_id = request.body.data.context.protocol.request.client_id;
 
 	try {
 		verifiedPickerContextJWT = njwt.verify(pickerContextJWT, process.env.PICKER_CLIENT_SECRET);
@@ -29,23 +30,54 @@ app.post("/tokenhook", (request, response) => {
 			}
 		}
 		response.send(tokenError);
+		return;
 	}
-	console.log('Verified JWT detail:')
-	console.log(verifiedPickerContextJWT)
+	
+	var consentedScopes = decodeURI(verifiedPickerContextJWT.body.scopes);
+	var consentedClient = verifiedPickerContextJWT.body.client_id
+	
+	console.log("Consented Scopes:" + consentedScopes)
+	console.log("Scopes sent to authz:" + scope)
+	
+	console.log("Consented App:" + consentedClient)
+	console.log("App sent to authz:" + client_id)
+	
+	//Validate to ensure that the scopes and client requested from the authz server do indeed match those selected during the consent process.
+	if(consentedScopes != scope || consentedClient != client_id) {
+		console.log("Request validation failed. Improper scope or client id.")
+		var tokenError = {
+			"error": {
+				errorSummary: "Invalid authorize request- the application and/or scopes sent to the authorization server do not match those the user consented to."
+			}
+		}
+		response.send(tokenError);
+		return;
+	}
+	
+	//If the patient_selection scope is included, it means that the patient_id was selected by the user at runtime
+	//Instead of being pulled from the user's profile.
+	if(scope.includes('patient_selection')) {
+		console.log('Verified JWT detail:')
+		console.log(verifiedPickerContextJWT)
 
-	console.log('Patient id: ' + verifiedPickerContextJWT.body.patient)
+		console.log('Patient id: ' + verifiedPickerContextJWT.body.patient)
 
-	var tokenUpdate = {
-		"commands": [{ 
-			"type": "com.okta.access.patch",
-			"value": [{ 
-				"op": "add",
-				"path": "/claims/launch_response_patient",
-				"value": verifiedPickerContextJWT.body.patient
-			}] 
-		}]
-	};
-	response.send(tokenUpdate);
+		var tokenUpdate = {
+			"commands": [{ 
+				"type": "com.okta.access.patch",
+				"value": [{ 
+					"op": "add",
+					"path": "/claims/launch_response_patient",
+					"value": verifiedPickerContextJWT.body.patient
+				}] 
+			}]
+		};
+		response.send(tokenUpdate);
+	}
+	else {  //No patient selection was requested, we don't need to put the patient in the token. Okta will do it via UD.
+		response.status(204).send();
+	}
+	
 })
 
 module.exports.tokenHook = serverless(app)
