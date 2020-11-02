732cb93f5f0833229bd0b3d5525fe4f5f6ebf7b9
Dan Cinnamon <dan.cinnamon@okta.com>
2020-11-02 22:56:38 +0000

    Updated the first name to match the actual patient data.

--- mock_patient_service.js
+++ mock_patient_service.js
@@ -9,7 +9,7 @@ app.get('/patientMockService', (request, response) => {
   response.send([
     {patient_id: '1440422', patient_name: 'Samuel Ortiz (31)'},
 	{patient_id: '92e12467-e074-4349-a6d4-e88be191b39b', patient_name: 'Amberly Bahringer (65)'},
-	{patient_id: 'c769110f-4d4e-4375-a706-c5d78f729544', patient_name: 'Nolan Ferry (63)'}
+	{patient_id: 'c769110f-4d4e-4375-a706-c5d78f729544', patient_name: 'Jann Ferry (63)'}
   ])
 })
 
