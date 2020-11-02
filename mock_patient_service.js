ac63796d952c40687bc38c71c248d04b8211905f
Dan Cinnamon <dan.cinnamon@okta.com>
2020-11-02 18:29:33 +0000

    Updated to add better demo example patients.

--- mock_patient_service.js
+++ mock_patient_service.js
@@ -7,9 +7,9 @@ const app = express()
 app.get('/patientMockService', (request, response) => {
   //TODO: Actually check the access token passed in.
   response.send([
-    {patient_id: '642726', patient_name: 'Peter Chalmers (45)'},
-    {patient_id: '1315163', patient_name: 'Jane Doe (25)'},
-    {patient_id: '1440422', patient_name: 'Samuel331 Ortiz186 (31)'}
+    {patient_id: '1440422', patient_name: 'Samuel Ortiz (31)'},
+	{patient_id: '92e12467-e074-4349-a6d4-e88be191b39b', patient_name: 'Amberly Bahringer (65)'},
+	{patient_id: 'c769110f-4d4e-4375-a706-c5d78f729544', patient_name: 'Nolan Ferry (63)'}
   ])
 })
 
